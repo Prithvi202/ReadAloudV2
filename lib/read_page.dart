a71bc05e823bddcc85df631604ccc17e8f18fdc8
the-dev7 <abhinandan.deb02@gmail.com>
2023-05-04 01:07:43 +0530

    Update read_page.dart

--- lib/read_page.dart
+++ lib/read_page.dart
@@ -12,7 +12,27 @@ class ReadPage extends StatefulWidget {
 
 class _ReadPageState extends State<ReadPage> {
 
-  final FlutterTts flutterTts = FlutterTts();
+  late FlutterTts flutterTts;
+  int start = 0;
+  int end = 0;
+
+  @override
+  void initState()
+  {
+    initTts();
+    super.initState();
+  }
+
+  initTts()
+  {
+    flutterTts = FlutterTts();
+    flutterTts.setProgressHandler((text, startOffset, endOffset, word) {
+      setState(() {
+        start = startOffset;
+        end = endOffset;
+      });
+    });
+  }
 
   @override
   void dispose()
@@ -26,6 +46,7 @@ class _ReadPageState extends State<ReadPage> {
 
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
+    double magicNumber = width * 0.003;
 
     bool isSpeaking = false;
 
@@ -46,6 +67,23 @@ class _ReadPageState extends State<ReadPage> {
       }
     }
 
+    pauseFunction(int start, int end) async {
+      await flutterTts.pause();
+      isSpeaking = false;
+      setState(() {
+        this.start = start;
+        this.end = end;
+      });
+    }
+
+    stopFunction() async {
+      await flutterTts.stop();
+      start = 0;
+      end = 0;
+      isSpeaking = false;
+      setState(() {});
+    }
+
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
@@ -63,34 +101,114 @@ class _ReadPageState extends State<ReadPage> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Container(
-              height: 200,
-              margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
-              child: ListView(
-                children: [
-                  SelectableText(
-                    widget.scannedText,
-                    textAlign: TextAlign.justify,
-                    style: TextStyle(
-                        fontSize: 18, color: Colors.white.withOpacity(0.6)),
-                    showCursor: true,
-                    cursorColor: Colors.grey[200],
-                    cursorRadius: const Radius.circular(6),
-                    scrollPhysics: const ClampingScrollPhysics(),
+              height: magicNumber * 200,
+              color: Colors.transparent,
+              padding: EdgeInsets.symmetric(horizontal: magicNumber * 30, vertical: magicNumber * 10),
+              child: SingleChildScrollView(
+                child: RichText(
+                textAlign: TextAlign.left,
+                text: TextSpan(style: TextStyle(fontSize: magicNumber * 16), children: <TextSpan>[
+                  TextSpan(
+                      text: widget.scannedText != null && start != 0
+                          ? widget.scannedText.substring(0, start)
+                          : "",
+                      style: TextStyle(color: Color.fromRGBO(150, 150, 150, 1))),
+                  TextSpan(
+                      text: widget.scannedText != null
+                          ? widget.scannedText.substring(start, end)
+                          : "",
+                      style: TextStyle(color: Color.fromRGBO(240, 240, 240, 1), fontWeight: FontWeight.w600)),
+                  TextSpan(
+                      text: widget.scannedText != null ? widget.scannedText.substring(end) : "",
+                      style: TextStyle(color: Color.fromRGBO(150, 150, 150, 1))),
+                    ]
                   ),
-                ],
-              ),
+                ),
+              )
             ),
 
-            const SizedBox(height: 20),
+            SizedBox(height: magicNumber * 30),
 
-            ElevatedButton(
+            /*ElevatedButton(
               onPressed: () {
                 speechFunction(widget.scannedText);
               }, 
               child: Text("Read")
-            ),
+            ),*/
 
-            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () => speechFunction(widget.scannedText),
+                  style: ElevatedButton.styleFrom(
+                    elevation: 10,
+                    backgroundColor: Colors.black,
+                    padding: EdgeInsets.all(magicNumber * 10),
+                    shadowColor: const Color.fromRGBO(255, 189, 66, 1),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(magicNumber * 5),
+                          bottomRight: Radius.circular(magicNumber * 5),
+                          topRight: Radius.circular(magicNumber * 25),
+                          bottomLeft: Radius.circular(magicNumber * 25)),
+                      side: const BorderSide(
+                        width: 2.0,
+                        color: Color.fromRGBO(255, 189, 66,
+                            0.558), //Color.fromRGBO(169, 167, 167, 1),//
+                      )
+                    )
+                  ), 
+                  child: Icon(Icons.play_arrow, size: magicNumber * 35, color: Color.fromRGBO(255, 189, 66, 1)),
+                ),
+                ElevatedButton(
+                  onPressed: () => pauseFunction(start, end),
+                  style: ElevatedButton.styleFrom(
+                    elevation: 10,
+                    backgroundColor: Colors.black,
+                    padding: EdgeInsets.all(magicNumber * 10),
+                    shadowColor: const Color.fromRGBO(255, 189, 66, 1),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(magicNumber * 5),
+                          bottomRight: Radius.circular(magicNumber * 5),
+                          topRight: Radius.circular(magicNumber * 25),
+                          bottomLeft: Radius.circular(magicNumber * 25)),
+                      side: const BorderSide(
+                        width: 2.0,
+                        color: Color.fromRGBO(255, 189, 66,
+                            0.558), //Color.fromRGBO(169, 167, 167, 1),//
+                      )
+                    )
+                  ), 
+                  child: Icon(Icons.pause_sharp, size: magicNumber * 35, color: Color.fromRGBO(255, 189, 66, 1)),
+                ),
+                ElevatedButton(
+                  onPressed: () => stopFunction(),
+                  style: ElevatedButton.styleFrom(
+                    elevation: 10,
+                    backgroundColor: Colors.black,
+                    padding: EdgeInsets.all(magicNumber * 10),
+                    shadowColor: const Color.fromRGBO(255, 189, 66, 1),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(magicNumber * 5),
+                          bottomRight: Radius.circular(magicNumber * 5),
+                          topRight: Radius.circular(magicNumber * 25),
+                          bottomLeft: Radius.circular(magicNumber * 25)),
+                      side: const BorderSide(
+                        width: 2.0,
+                        color: Color.fromRGBO(255, 189, 66,
+                            0.558), //Color.fromRGBO(169, 167, 167, 1),//
+                      )
+                    )
+                  ), 
+                  child: Icon(Icons.stop_sharp, size: magicNumber * 35, color: Color.fromRGBO(255, 189, 66, 1)),
+                ),
+              ],
+            )
+            
           ],
         ),
       ),
